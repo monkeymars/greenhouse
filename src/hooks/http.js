1f27da74e814fa2724fcfff5afcca8233de17145
Gery Wibowo <akun.gery@gmail.com>
2019-05-31 02:33:11 +0700

    remove unused function

--- src/hooks/http.js
+++ src/hooks/http.js
@@ -1,17 +1,12 @@
 import { useState, useEffect } from 'react'
-import MOCK_DATA from '../resource/MOCK_DATA.json';
+import axios from 'axios';
 
 export const useHttp = (url, depedencies) => {
     const [isLoading, setIsLoading] = useState(false);
     const [fetchData, setFetchData] = useState(null);
     useEffect(() => {
         setIsLoading(true);
-        fetch(url).then((response) => {
-            if (!response) {
-                throw new Error('Failed to fetch!');
-            }
-            return response.json();
-        }).then(data => {
+        axios.get(url).then(data => {
             setIsLoading(false);
             setFetchData(data);
         }).catch(err => {
@@ -22,12 +17,4 @@ export const useHttp = (url, depedencies) => {
 
     return [isLoading, fetchData];
 
-}
-
-/**
- * MockData
- * @returns {Object} sample products
- */
-export const MockData = () => {
-    return MOCK_DATA;
 }
