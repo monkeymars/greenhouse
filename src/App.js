e5b7086a58127e94e6c8fe6ea85c048be3eeca93
Gery Wibowo <akun.gery@gmail.com>
2019-05-31 01:35:13 +0700

    packages

--- src/App.js
+++ src/App.js
@@ -1,100 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Card, Icon, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
-import { Grid } from 'semantic-ui-react';
-import { Container, Header } from 'semantic-ui-react';
-import { Input, Menu } from 'semantic-ui-react';
-import { Divider } from 'semantic-ui-react';
+import Header from './components/Header';
+import Catalog from './components/Catalog';
 
 function App() {
   return (
     <div className="App">
-      <div className="header-nav">
-        <Container>
-          <div className="logo-box" style={{textAlign:'center'}}>
-            <span className="logo">Market Logo</span>
-            <span className="cart-box"><Icon link name='cart'/></span>
-          </div>
-        </Container>
-      </div>
-
-      <Container>
-        <div className="product-list">
-          <Grid>
-          <Grid.Row columns={4}>
-            <Grid.Column>
-              <Card>
-                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Matthew</Card.Header>
-                  <Card.Meta>
-                    <span className='date'>Rp15.000,-</span>
-                  </Card.Meta>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name='cart' />
-                    <strong>Add to Cart</strong>
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column>
-              <Card>
-                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Matthew</Card.Header>
-                  <Card.Meta>
-                    <span className='date'>Rp15.000,-</span>
-                  </Card.Meta>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name='cart' />
-                    <strong>Add to Cart</strong>
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column>
-              <Card>
-                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Matthew</Card.Header>
-                  <Card.Meta>
-                    <span className='date'>Rp15.000,-</span>
-                  </Card.Meta>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name='cart' />
-                    <strong>Add to Cart</strong>
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column>
-              <Card>
-                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Matthew</Card.Header>
-                  <Card.Meta>
-                    <span className='date'>Rp15.000,-</span>
-                  </Card.Meta>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name='cart' />
-                    <strong>Add to Cart</strong>
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        </div>
-      </Container>
+      <Header/>
+      <Catalog/>
     </div>
   );
 }
