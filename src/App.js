a7e0ceae7109c19d657975a98bfdf009461cd563
Gery Wibowo <akun.gery@gmail.com>
2019-05-30 19:28:20 +0700

    draft layout homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card, Icon, Image } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import { Grid } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
+import { Input, Menu } from 'semantic-ui-react';
+import { Divider } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-nav">
+        <Container>
+          <div className="logo-box" style={{textAlign:'center'}}>
+            <span className="logo">Market Logo</span>
+            <span className="cart-box"><Icon link name='cart'/></span>
+          </div>
+        </Container>
+      </div>
+
+      <Container>
+        <div className="product-list">
+          <Grid>
+          <Grid.Row columns={4}>
+            <Grid.Column>
+              <Card>
+                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Matthew</Card.Header>
+                  <Card.Meta>
+                    <span className='date'>Rp15.000,-</span>
+                  </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='cart' />
+                    <strong>Add to Cart</strong>
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card>
+                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Matthew</Card.Header>
+                  <Card.Meta>
+                    <span className='date'>Rp15.000,-</span>
+                  </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='cart' />
+                    <strong>Add to Cart</strong>
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card>
+                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Matthew</Card.Header>
+                  <Card.Meta>
+                    <span className='date'>Rp15.000,-</span>
+                  </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='cart' />
+                    <strong>Add to Cart</strong>
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card>
+                <Image src='https://dummyimage.com/250x250.png/ff4444/ffffff' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Matthew</Card.Header>
+                  <Card.Meta>
+                    <span className='date'>Rp15.000,-</span>
+                  </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='cart' />
+                    <strong>Add to Cart</strong>
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        </div>
+      </Container>
     </div>
   );
 }
