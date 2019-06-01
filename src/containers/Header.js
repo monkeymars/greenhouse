749746782325fbe8909d95fc914d8834f20ddd96
Gery Wibowo <akun.gery@gmail.com>
2019-06-02 00:18:37 +0700

    Popup cart items and total price

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,18 +1,70 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Icon } from 'semantic-ui-react';
+import { Container, Icon, Popup, Button, List, Image } from 'semantic-ui-react';
 import { removeCart } from '../actions/cartAction';
 
 class Header extends React.Component {
+
+    cartTotal(items) {
+        return items.reduce(function (total, item) {
+            return total + item.price;
+        }, 0);
+    }
     render() {
+        const style = {
+            borderRadius: 0,
+            padding: '2em',
+            width: '400px'
+        }
+        const cartItems = this.props.cart.listProduct
+        const popupTrigger = (<span><Icon link name='cart'/> {cartItems.length}</span>)
+        let popupContent = <div></div>;
+        if (cartItems.length) {
+            popupContent = (
+                <div>
+                    <List divided relaxed>
+                        {
+                            cartItems.map((item) => {
+                                return (
+                                    <List.Item key={item.id}>
+                                        <List.Content floated='right'>{item.price}</List.Content>
+                                        <List.Content>
+                                            <strong>{item.product_name}</strong>
+                                        </List.Content>
+                                    </List.Item>
+                                )
+                            })
+                        }
+                        <List.Item>
+                            <List.Content floated='right'>{this.cartTotal(cartItems)}</List.Content>
+                            <List.Content>
+                                <strong>{`Total`}</strong>
+                            </List.Content>
+                        </List.Item>
+                    </List>
+                    <div style={{textAlign:'center'}}>
+                        <Button color='red' content='Checkout' />
+                    </div>
+                </div>
+            )
+        }
+
         return (
             <div className="header-nav">
                 <Container>
                     <div className="logo-box" style={{textAlign:'center'}}>
                         <span className="logo">Market Logo</span>
                         <span className="cart-box">
-                            <Icon link name='cart'/>
-                            {this.props.cart.listProduct.length}
+                            <React.Fragment>
+                                <Popup flowing wide
+                                    offset='0, 10px'
+                                    trigger={popupTrigger}
+                                    content={popupContent}
+                                    position='bottom left'
+                                    style={style}
+                                    on='click'
+                                    />
+                            </React.Fragment>
                         </span>
                     </div>
                 </Container>
